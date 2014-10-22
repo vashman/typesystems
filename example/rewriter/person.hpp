908337e6dcb723723d1268331eff81c1241e7bb4
Sunny S <3030712@gmail.com>
2014-10-21 21:32:35 -0700

    added license header.

--- example/rewriter/person.hpp
+++ example/rewriter/person.hpp
@@ -1,5 +1,10 @@
 // A custom class used to demostrate rewriting.
 
+//          Copyright Sundeep S. Sangha 2013 - 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include <string>
 
 class person {
