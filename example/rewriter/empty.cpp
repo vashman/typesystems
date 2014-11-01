e40f47555f43fd5877db6f1fcfbc0b14f2828f23
Sunny S <3030712@gmail.com>
2014-10-31 22:06:00 -0700

    changed char ptr to std::string, fixed syntax.

--- example/rewriter/empty.cpp
+++ example/rewriter/empty.cpp
@@ -1,32 +1,43 @@
+//
+
 #include <vector>
 #include <iostream>
-#include "../include/typebuffer.hpp"
-#include "person.hpp"
+#include <string>
+#include "../../include/typebuffer.hpp"
+#include "../../include/get_rewriter.hpp"
 #include "rwperson.hpp"
-#include "../include/get_rewriter.hpp"
 
+using typesystems::typebuffer_container;
+using typesystems::set_typebuffer;
+using typesystems::use_typebuffer;
+using typesystems::typebuffer_interface;
 using typesystems::get_rewriter;
 
-int main(){
+int main() try {
 typebuffer_container con;
-set_typebuffer<int, std::vector<int> >(con);
-set_typebuffer<char *, std::vector<> >(con);
+set_typebuffer<int const, std::vector<int> >(con);
+set_typebuffer<std::string, std::vector<std::string> >(con);
 
 /* fill the infomation directly */
-typebuffer_interface<int> & buf1 = use_typebuffer<int>(con);
-buf.push(24);
-buf.push(32);
-buf.push(42);
+typebuffer_interface<int const> & buf1 = use_typebuffer<int const>(con);
+buf1.push(24);
+buf1.push(32);
+buf1.push(42);
 
 /* add only 2 items so the extra int will be ignored */
-typebuffer_interface<> & buf2 = use_typebuffer<>(con);
+typebuffer_interface<std::string> & buf2
+                    = use_typebuffer<std::string>(con);
 buf2.push("probi");
 buf2.push("otic");
 
 get_rewriter<person> * rwg = new person_rw_get();
 
 /**/
-bool rv = rwg->empty<person>();
+bool rv = rwg->empty(con);
 
 return 0;
+}
+  catch (...) {
+std::cerr << "Unknown exception thrown.\n";
+return 1;
 }
