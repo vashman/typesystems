9fe3b5309d18fbe39e322c0824a674e84438b711
Sunny S <3030712@gmail.com>
2014-11-12 03:21:10 -0800

    clerified the example with comments.
    * seperated the variable decleration and assignment.
    * commented each line.

--- example/explicit_typeid/create.cpp
+++ example/explicit_typeid/create.cpp
@@ -11,7 +11,12 @@ using typesystems::explicit_typeid;
 using typesystems::explicit_typeid_type;
 
 int main(){
-explicit_typeid_type id = explicit_typeid<float>::raw_typeid();
+
+/* A explicit_typeid_type variable can hold explicit_ids of any type */
+explicit_typeid_type id;
+
+/* assigns the id of the float type to the variable id. */
+id = explicit_typeid<float>::raw_typeid();
 
 return 0;
 }
