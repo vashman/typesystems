90c3d5700d7517e95497966cb274d1e5f72e85d5
Sunny Sangha <3030712@gmail.com>
2017-01-15 01:12:39 -0800

    ignore debug folder

--- src/qualified_typeid.cpp
+++ src/qualified_typeid.cpp
@@ -1,5 +1,10 @@
 //
 
+//          Copyright Sundeep S. Sangha 2013 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef TYPESYSTEMS_QUALIFIED_TYPEID_CPP
 #define TYPESYSTEMS_QUALIFIED_TYPEID_CPP
 
